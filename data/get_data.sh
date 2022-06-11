63896170c785372c3525c12d9061982e9bfddaae
Matic Fučka <mf4782@hpc-login2.arnes.si>
2022-06-11 12:17:25 +0200

    Popravil get_data skripto

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,8 +1,10 @@
-data_links = ()
-file_names = ("geo_location.csv","weather.csv", "school.csv", "poi.csv")
+data_links=("https://data.cityofnewyork.us/api/views/8rma-cm9c/rows.csv?accessType=DOWNLOAD" "https://data.cityofnewyork.us/api/views/qdq3-9eqn/rows.csv?accessType=DOWNLOAD" "https://data.cityofnewyork.us/api/views/p6h4-mpyy/rows.csv?accessType=DOWNLOAD" "https://data.cityofnewyork.us/api/views/t95h-5fsr/rows.csv?accessType=DOWNLOAD")
+file_names=("geo_location.csv" "weather.csv" "school.csv" "attraction.csv")
 
-for i in {!data_links[@]}
+for i in "${!data_links[@]}"
 do
-    wget data_links[i]
-    mv "" filenames[i]
-end
+	echo "Doing the "${filnames[$i]}" file"
+	wget -q ${data_links[$i]}
+	mv "rows.csv?accessType=DOWNLOAD" ${filenames[$i]}
+	echo "Done"
+done
